6931a2a8b3ec59b3712ca57827d6922cdc0875b3
Giles Bradford <hockeyftw27@gmail.com>
2020-04-10 21:39:16 -0400

    initial commit

---
+++ src/main/java/ZCW/dotaapi/DotaApiApplication.java
@@ -0,0 +1,13 @@
+package ZCW.dotaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DotaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ZCW/dotaapi/DotaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ZCW.dotaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DotaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
